21bd1842e8f76a55c9c857b4318b8f82ba82a834
MrFrangipane <contact@frangipane.me>
2023-12-05 16:31:36 +0100

    test(build on pi): debug mode

--- rpi4-build.sh
+++ rpi4-build.sh
@@ -1,3 +1,3 @@
 export JUCE_PATH="${HOME}/JUCE"
-cmake -B build -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang
+cmake -B build -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DCMAKE_BUILD_TYPE=Debug
 cmake --build build -j4
